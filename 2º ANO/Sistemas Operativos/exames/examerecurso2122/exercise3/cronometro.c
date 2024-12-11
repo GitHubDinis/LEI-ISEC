288420813a2005a5e47b1920c5aad4821b4547cc
Dinis Falcão <74770089+GitHubDinis@users.noreply.github.com>
2024-12-11 00:43:36 +0000

    Update cronometro.c

--- 2º ANO/Sistemas Operativos/exames/examerecurso2122/exercise3/cronometro.c
+++ 2º ANO/Sistemas Operativos/exames/examerecurso2122/exercise3/cronometro.c
@@ -4,63 +4,80 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <time.h>
 
 int getSegundos(){
-	return 10; // EXEMPLO
+    return (int)time(NULL);
 }
 
 int main(int argc, char **argv){
-	int tempo_inicial, tempo_final, tempo_max;
-	int status, nComandos;
+    int tempo_inicial, tempo_final, tempo_max;
+    int status, nComandos;
 
-	if(argc < 11){
-		nComandos = argc - 1;
-	} else {
-		return 0;
-	}
+    if(argc < 2){ 
+        printf("Número insuficiente de argumentos.\n");
+        return 1;
+    }
 
-	if(getenv("TEMPMAX") != NULL){
-		tempo_max = atoi(getenv("TEMPMAX"));
-	} else{
-		return 0;
-	}
+    nComandos = argc - 1; 
+	
+    if(getenv("TEMPMAX") != NULL){
+        tempo_max = atoi(getenv("TEMPMAX"));
+    } else {
+        tempo_max = 0; 
+    }
 
-	int FDPF[nComandos - 1][2];
-	for(int i = 0; i < nComandos - 1; i++){
-		pipe(FDPF[i]);
-	}
+    int FDPF[nComandos - 1][2];
+    for(int i = 0; i < nComandos - 1; i++){
+        if(pipe(FDPF[i]) == -1){
+            perror("[ERROR] Pipe creation failed");
+            return 1;
+        }
+    }
 
-	tempo_inicial = getSegundos();
+    tempo_inicial = getSegundos();
 
-	for (int i = 0; i < nComandos - 1; i++) {
-        	pid_t pid = fork();
-		if(pid == 0){
-			close(STDIN_FILENO);
-			dup(FDPF[i][0]);
-			close(STDOUT_FILENO);
-			dup(FDPF[i][1]);
+    for (int i = 0; i < nComandos; i++) {
+        pid_t pid = fork();
 
-			for(int j = 0; j < nComandos - 1; j++){
-				close(FDPF[j][0]);
-				close(FDPF[j][1]);
-			}
+        if(pid == 0){ 
+            if(i > 0) {
+                close(STDIN_FILENO);
+                dup(FDPF[i - 1][0]);
+            }
 
-			execlp(argv[i + 1], argv[i + 1], NULL);
-		} else if(pid == -1){
-			printf("[ERROR]\n");
-			return 0;
-		}
-	}
+            if(i < nComandos - 1) { 
+                close(STDOUT_FILENO);
+                dup(FDPF[i][1]);
+            }
 
-	for(int i = 0; i < nComandos - 1; i++){
-                close(FDPF[i][0]);
-                close(FDPF[i][1]);
-	}
+            for(int j = 0; j < nComandos - 1; j++){
+                close(FDPF[j][0]);
+                close(FDPF[j][1]);
+            }
 
-	for(int i = 0; i < nComandos - 1; i++){
-		wait(&status);
-	}
+            execlp(argv[i + 1], argv[i + 1], NULL);
+        } else if(pid == -1){
+            perror("[ERROR] fork failed");
+            return 1;
+        }
+    }
 
-	tempo_final = getSegundos();
-	printf("Cronómetro: %d", tempo_final - tempo_inicial);
+    for(int i = 0; i < nComandos - 1; i++){
+        close(FDPF[i][0]);
+        close(FDPF[i][1]);
+    }
+
+    for(int i = 0; i < nComandos; i++){
+        wait(&status);
+    }
+
+    tempo_final = getSegundos();
+    printf("Cronómetro: %d segundos\n", tempo_final - tempo_inicial);
+
+    if(tempo_max > 0 && (tempo_final - tempo_inicial) > tempo_max) {
+        printf("Tempo máximo excedido!\n");
+    }
+
+    return 0;
 }
